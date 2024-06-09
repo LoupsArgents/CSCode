2b1244d927745844d6b1f0531990dea31a412e7b
silverwolves20153 <silverwolvesftc@gmail.com>
2024-06-09 15:51:06 -0400

    optical odo is apparently absolute fire

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpticalOdoTesting.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class OpticalOdoTesting extends CSYorkDF {
+    SparkFunOTOS opticalOdo;
+    public void runOpMode(){
+        initializeHardware();
+        opticalOdo = hardwareMap.get(SparkFunOTOS.class, "optical");
+        //configure the odo
+        opticalOdo.setLinearUnit(SparkFunOTOS.LinearUnit.INCHES);
+        opticalOdo.setAngularUnit(SparkFunOTOS.AngularUnit.DEGREES);
+        SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, -0.66, 180);
+        opticalOdo.setOffset(offset);
+        opticalOdo.setLinearScalar(0.986333);
+        opticalOdo.setAngularScalar(0.995088);
+        opticalOdo.calibrateImu();
+        opticalOdo.resetTracking();
+        telemetry.addLine("Ready to run");
+        telemetry.update();
+        waitForStart();
+        int forwardStartTicks = forwardOdo.getCurrentPosition();
+        goStraight(.5, 40, 0.0);
+        while(opModeIsActive()){
+            telemetry.addData("Angle", opticalOdo.getPosition().h);
+            telemetry.addData("Forward", opticalOdo.getPosition().y);
+            telemetry.addLine("Wheels say " + newInchesTraveled(forwardStartTicks, forwardOdo.getCurrentPosition()));
+            telemetry.update();
+        }
+    }
+}
