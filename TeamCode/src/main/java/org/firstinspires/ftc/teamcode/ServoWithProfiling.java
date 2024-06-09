e7289accd3acf2432bff106396a7ae8ddf7eaf76
silverwolves20153 <silverwolvesftc@gmail.com>
2024-06-09 16:14:24 -0400

    Finished new servo motion profiling class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoWithProfiling.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoWithProfiling.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.ServoImplEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -17,6 +19,7 @@ public class ServoWithProfiling {
     private boolean isMoving = false;
     private double startPosEnc;
     private double endPosEnc;
+    private double endPosServo;
     private double setTo;
     private double m1;
     private double m2;
@@ -24,6 +27,7 @@ public class ServoWithProfiling {
     private double b2;
     private double msForProfile;
     private double percentStartProfiling;
+    private boolean hasStartedProfiling = false;
     ElapsedTime t = new ElapsedTime();
 
     public ServoWithProfiling(ServoImplEx se, AnalogInput a, double m11, double m22, double b11, double b22, double ms, double per) {
@@ -38,10 +42,6 @@ public class ServoWithProfiling {
         initialPos = currentPos;
         percentStartProfiling = per;
         arr.add(this);
-    }
-
-    public ServoWithProfiling(String se, String a, double m11, double m22, double b11, double b22, double ms, double per) {
-        this(hardwareMap.get(ServoImplEx.class, se), hardwareMap.get(AnalogInput.class, a), m11, m22, b11, b22, ms, per);
     }
 
     public double servoToEncoderPos(double servoPos) {
@@ -73,16 +73,29 @@ public class ServoWithProfiling {
     }
     public void setEndPosEnc(double epe) {
         this.endPosEnc = epe;
-        isMoving = true;
+        endPosServo = encoderToServoPos(epe);
     }
     public double getEndPosEnc() {
         return this.endPosEnc;
+    }
+    public void setEndPosServo(double eps) {
+        endPosServo = eps;
+        endPosEnc = servoToEncoderPos(eps);
     }
     public boolean getIsMoving() {
         return isMoving;
     }
     public void setIsMoving(boolean im) {
         isMoving = im;
+        if (im) {
+            hasStartedProfiling = false;
+        }
+    }
+    public void setMsForProfile(double msp) {
+        msForProfile = msp;
+    }
+    public double getMS() {
+        return t.milliseconds();
     }
 
     public static void updateAllPos() {
@@ -99,14 +112,25 @@ public class ServoWithProfiling {
 
     public void profile() { //also updates isMoving
         isMoving = true;
+        if (Math.abs(currentPos - endPosEnc) < 1) {
+            isMoving = false;
+            s.setPosition(endPosServo);
+            setTo = endPosServo;
+            return;
+        }
         if (startPosEnc < endPosEnc) {
-            if (currentPos > startPosEnc + (endPosEnc - startPosEnc) * percentStartProfiling) {
+            double switchToProfiling = startPosEnc + (endPosEnc - startPosEnc) * percentStartProfiling;
+            if (hasStartedProfiling || currentPos > switchToProfiling) {
                 //we want to be profiling
-                double idealEncoderPos = startPosEnc + (t.milliseconds()/msForProfile)*(endPosEnc - startPosEnc);
+                hasStartedProfiling = true;
+                double idealEncoderPos = switchToProfiling + (t.milliseconds()/msForProfile)*(endPosEnc - switchToProfiling);
+                if (idealEncoderPos < currentPos) {
+                    idealEncoderPos = currentPos + 2.5;
+                }
                 double idealServoPos = encoderToServoPos(idealEncoderPos);
                 s.setPosition(idealServoPos);
                 setTo = idealServoPos;
-                if (t.milliseconds() > msForProfile || currentPos > endPosEnc) {
+                if (t.milliseconds() > msForProfile || Math.abs(currentPos - endPosEnc) < 1) {
                     s.setPosition(encoderToServoPos(endPosEnc));
                     setTo = encoderToServoPos(endPosEnc);
                     isMoving = false;
@@ -118,13 +142,18 @@ public class ServoWithProfiling {
                 t.reset();
             }
         } else { //startPos > endPos
-            if (currentPos < startPosEnc - (startPosEnc - endPosEnc) * percentStartProfiling) {
+            double switchToProfiling = startPosEnc - (startPosEnc - endPosEnc) * percentStartProfiling;
+            if (hasStartedProfiling || currentPos < switchToProfiling) {
+                hasStartedProfiling = true;
                 //we want to be profiling
-                double idealEncoderPos = startPosEnc - (t.milliseconds()/msForProfile)*(startPosEnc - endPosEnc);
+                double idealEncoderPos = switchToProfiling - (t.milliseconds()/msForProfile)*(switchToProfiling - endPosEnc);
+                if (idealEncoderPos > currentPos) {
+                    idealEncoderPos = currentPos - 2.5;
+                }
                 double idealServoPos = encoderToServoPos(idealEncoderPos);
                 s.setPosition(idealServoPos);
                 setTo = idealServoPos;
-                if (t.milliseconds() > msForProfile || currentPos < endPosEnc) {
+                if (t.milliseconds() > msForProfile || Math.abs(currentPos - endPosEnc) < 1) {
                     s.setPosition(encoderToServoPos(endPosEnc));
                     setTo = encoderToServoPos(endPosEnc);
                     isMoving = false;
