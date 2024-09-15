bb83df2d4eeea573d9f9d866c10e4171cce13ecc
silverwolves20153 <silverwolvesftc@gmail.com>
2024-09-15 16:11:09 -0400

    Added go-past-the-point mode and adjustable anti-stall timeout to moveTo() function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/centerstage/OpticalOdoTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/centerstage/OpticalOdoTesting.java
@@ -10,16 +10,23 @@ public class OpticalOdoTesting extends CSYorkDF {
         telemetry.addLine("Ready to run");
         telemetry.update();
         waitForStart();
-        for(int i = 0; i < 180; i += 15) {
+        /*for(int i = 0; i < 180; i += 15) {
             double x = Math.cos(Math.toRadians(i)) * 12;
             double y = Math.sin(Math.toRadians(i)) * 12;
-            moveTo(.5, new Position(x, y, 0.0), true);
+            moveTo(.7, new Position(x, y, 0.0), true);
             wait(500);
-            moveTo(.5, new Position(0, 0, 0.0), true);
+            moveTo(.7, new Position(0, 0, 0.0), true);
             wait(500);
-        }
+        }*///  |\
+        //move \|
+        moveTo(.5, new Position(0, 10, 0.0), false, false);
+        moveTo(.5, new Position(10, 20, 0.0), true);
+        sleep(500);
+        moveTo(.5, new Position(10, 10, 0.0), false, false);
+        moveTo(.5, new Position(0, 0, 0.0), true);
         while(opModeIsActive()){
-
+            telemetry.addData("Status", "Finished");
+            telemetry.update();
         }
     }
 }
